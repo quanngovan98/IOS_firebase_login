8382a02ff965b262648d1c04452ffb9f6eb08aff
ngovanquan <quannv@sendo.vn>
2020-06-16 09:52:17 +0700

    Initial Commit

---
+++ CustomLoginDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomLoginDemo
+//
+//  Created by Ngo Van Quan on 6/16/20.
+//  Copyright © 2020 Ngo Van Quan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
